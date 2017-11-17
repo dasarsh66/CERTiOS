758db8554ed5b0032495aeccfd14ccd13a4ba6dc
student <student@nw44812.nwmissouri.edu>
2017-11-16 19:24:45 -0600

    Inserting sample data to mlab  successfull --dasarsh

--- CERT/RegisterViewController.swift
+++ CERT/RegisterViewController.swift
@@ -10,6 +10,16 @@ import UIKit
 
 class RegisterViewController: UIViewController {
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
     @IBOutlet weak var RegisterFirstName: UITextField!
     @IBOutlet weak var RegisterLastName: UITextField!
     @IBOutlet weak var RegisterContact: UITextField!
@@ -18,23 +28,52 @@ class RegisterViewController: UIViewController {
     @IBOutlet weak var RegisterRegister: UIButton!
     
     @IBAction func RegisterGoBack(_ sender: Any) {
-        self.dismiss(animated: true, completion: nil)
-
-    
+        //declare parameter as a dictionary which contains string as key and value combination. considering inputs are valid
+        
+        let parameters = ["id": 13, "name": "jack"] as [String : Any]
+        
+        //create the url with URL
+        let url = URL(string: "https://api.mlab.com/api/1/databases/cert/collections/my-coll?apiKey=F4GTF2uFESKCFCx7q87ZtFmrhtL-A1-j")! //change the url
+        
+        //create the session object
+        let session = URLSession.shared
+        
+        //now create the URLRequest object using the url object
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST" //set http method as POST
+        
+        do {
+            request.httpBody = try JSONSerialization.data(withJSONObject: parameters, options: .prettyPrinted) // pass dictionary to nsdata object and set it as request body
+        } catch let error {
+            print(error.localizedDescription)
+        }
+        
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        
+        //create dataTask using the session object to send data to the server
+        let task = session.dataTask(with: request as URLRequest, completionHandler: { data, response, error in
+            
+            guard error == nil else {
+                return
+            }
+            
+            guard let data = data else {
+                return
+            }
+            
+            do {
+                //create json object from data
+                if let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String: Any] {
+                    print(json)
+                    // handle json...
+                }
+            } catch let error {
+                print(error.localizedDescription)
+            }
+        })
+        task.resume()
     }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
     /*
     // MARK: - Navigation
 
